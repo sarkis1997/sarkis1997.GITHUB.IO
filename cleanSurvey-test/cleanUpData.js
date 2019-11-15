2ea921d8dfa6458ab62bf52c394912f5d3b23cf9
Sarkis Moeradjan <smoeradjan@gmail.com>
2019-11-15 11:36:47 +0100

    new  copy

--- cleanSurvey-test/cleanUpData.js
+++ cleanSurvey-test/cleanUpData.js
@@ -80,36 +80,55 @@ Geen studieschuld
 Meer dan 25000
 Geen studieschuld
 Geen studieschuld
-10000-15000`
+10000-15000`;
 
-/**
- * @return {string}
- */
-function CleanData(data) {
-    const array = data.split(/\n/)
+function cleanData(data) {
+	// make an array of the string and split it on new line
+    const array = data.split(/\n/);
+    // make a new array in the array that splits on '-' to separate 2 values
     const items = array.map(item => {
         return item.split('-')
     });
-    
+
+    // make a new array of the item array
     return items.map(item => {
+    	//check the length of each array, if it's 1 do more checks
         if (item.length == 1) {
-            return item[0]
+        	// if value is empty or 'geen studieschuld' return 0
+	        if (item == "" || item == "Geen studieschuld") {
+		        return item = 0;
+	        }
+	        // keep this text
+	        else if (item == "Hoe hoog is je studieschuld op dit moment?") {
+	        	return item;
+	        }
+	        // else return 27500 (more then 25000)
+	        return item = 27500
         }
 
-        // If it's not one item => 'Geen studieschuld or something',
-        // it's a range of numbers (a.k.a. [[1500][2000]])
+        // If it's more than 1 item, it's a range of numbers [[1500][2000]]
         let range = item.map(value => {
-            // Because quirks
+            // Because quirks (like more then 2 values seperated with ',' in the range)
             if (value.includes(', ')) {
                 return Number(value.split(', ')[0]) // Throw away second value after ,
             }
-
             return Number(value) // array of numbers
-        })
+        });
 
-
+        // here gets the average calculated.
+	    // accumulating the values and dividing by the length of the range
         return range.reduce((accumulator, currentNumber) => {
-            return (accumulator + currentNumber) / range.length
-        }).toString().replace('.', ',') //Make string of number + replaces the decimal
-    }).join("\r\n") //makes string (with new line) of the items array
+            let average = (accumulator + currentNumber) / range.length;
+            // check if it's a whole number, if it's not fix to max 2 decimals
+            if (average % 1 !== 0) {
+	            return average.toFixed(2)
+            }
+            return average
+        })
+	        .toString() //Make strings of numbers
+	        .replace('.', ',') //] + replaces all decimals
+    })
+	    .join("\r\n") //makes one string (with new line) of the items array
 }
+
+console.log(cleanData(datastring));
